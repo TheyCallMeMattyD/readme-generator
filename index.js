77e30a31dc9286a25fe4047a812602803535424b
TheMattyDGWU <themattydgwu@gmail.com>
2020-10-14 16:16:43 -0400

    styling updates

--- index.js
+++ index.js
@@ -6,55 +6,55 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
     return inquirer.prompt([{
-        type: "input",
-        message: "Enter your project title:",
-        name: "Title"
-    },
-    {
-        type: "input",
-        message: "Enter a brief project description:",
-        name: "Description"
-    },
-    {
-        type: "input",
-        message: "Enter the installation instructions:",
-        name: "Installation"
-    },
-    {
-        type: "input",
-        message: "Enter usage information:",
-        name: "Usage"
-    },
-    {
-        type: "input",
-        message: "Enter contribution guidelines:",
-        name: "Contributing"
-    },
-    {
-        type: "input",
-        message: "Enter testing instructions:",
-        name: "Tests"
-    },
-    {
-        type: "list",
-        message: "Choose a license for your application:",
-        name: "License",
-        choices: [
-            "GPLv3",
-            "MIT",
-            "Apache2.0"
-        ]
-    },
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "GitHub"
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "Email"
-    }
+            type: "input",
+            message: "Enter your project title:",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Enter a brief project description:",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "Enter the installation instructions:",
+            name: "Installation"
+        },
+        {
+            type: "input",
+            message: "Enter usage information:",
+            name: "Usage"
+        },
+        {
+            type: "input",
+            message: "Enter contribution guidelines:",
+            name: "Contributing"
+        },
+        {
+            type: "input",
+            message: "Enter testing instructions:",
+            name: "Tests"
+        },
+        {
+            type: "list",
+            message: "Choose a license for your application:",
+            name: "License",
+            choices: [
+                "GPLv3",
+                "MIT",
+                "Apache2.0"
+            ]
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "GitHub"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "Email"
+        }
     ]);
 }
 
@@ -68,10 +68,10 @@ function generateReadMe(answers) {
 
 # ${answers.Title}
 ## Description
-${answers.Description}
+>${answers.Description}
   
 ## Table of Contents
-1. [Title](#Title)
+>1. [Title](#Title)
 2. [Description](#Description)
 3. [Installation](#Installation)
 4. [Usage](#Usage)
@@ -81,34 +81,36 @@ ${answers.Description}
 8. [Questions?](#Questions?)
   
 ## Installation
-${answers.Installation}
+>${answers.Installation}
   
 ## Usage
-${answers.Usage}
+>${answers.Usage}
   
 ## License [![License: ${answers.License}](https://img.shields.io/badge/License-${answers.License}-blue.svg)](https://opensource.org/licenses/${answers.License})
-This application is covered under the [${answers.License} License](https://opensource.org/licenses/${answers.License})
+>This application is covered under the [${answers.License} License](https://opensource.org/licenses/${answers.License})
   
 ## Contributing
-${answers.Contributing}
+>${answers.Contributing}
+
 ## Tests
-${answers.Tests}
+>${answers.Tests}
   
 ## Questions?
-Please direct all questions to:ß ${answers.Email}
-Check out my [GitHub Profile](https://github.com/${answers.GitHub}) 
+>Please direct all questions to:
+${answers.Email}<br/>
+Check out my [GitHub Profile]<br/>(https://github.com/${answers.GitHub}) 
     `;
 }
 
 promptUser()
-    .then(function (answers) {
+    .then(function(answers) {
         const readMe = generateReadMe(answers);
 
         return writeFileAsync("generated-README.md", readMe);
     })
-    .then(function () {
+    .then(function() {
         console.log("Successfully created generated-README.md");
     })
-    .catch(function (err) {
+    .catch(function(err) {
         console.log(err)
     });
