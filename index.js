ed8abc8c712ff6ca57b239151570a6b3acdd4e55
Matthew Daniels <themattydgwu@gmail.com>
2020-10-13 21:49:30 -0400

    added last commit badge

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ function generateReadMe(answers) {
     return `
 [![GitHub issues](https://img.shields.io/github/issues/TheyCallMeMattyD/readme-generator?style=for-the-badge)](https://github.com/TheyCallMeMattyD/readme-generator/issues) &nbsp; &nbsp; &nbsp;
 [![GitHub stars](https://img.shields.io/github/stars/TheyCallMeMattyD/readme-generator?style=for-the-badge)](https://github.com/TheyCallMeMattyD/readme-generator/stargazers) &nbsp; &nbsp; &nbsp;
+![GitHub last commit](https://img.shields.io/github/last-commit/theycallmemattyd/readme-generator?style=for-the-badge)
+
 
 # ${answers.Title}
 ## Description
