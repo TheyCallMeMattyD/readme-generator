9f0c330a7592f7b55f7a6582459ccd5f83194c17
TheMattyDGWU <themattydgwu@gmail.com>
2020-10-14 19:41:17 -0400

    test badge

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function generateReadMe(answers) {
 [![GitHub stars](https://img.shields.io/github/stars/TheyCallMeMattyD/readme-generator?style=for-the-badge)](https://github.com/TheyCallMeMattyD/readme-generator/stargazers) &nbsp;
 ![GitHub last commit](https://img.shields.io/github/last-commit/theycallmemattyd/readme-generator?style=for-the-badge) &nbsp;
 ![GitHub commit activity](https://img.shields.io/github/commit-activity/m/theycallmemattyd/readme-generator?style=for-the-badge) 
-
+<img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/m/theycallmemattyd/readme-generator?style=for-the-badge">
 # ${answers.Title}
 ## Description
 >${answers.Description}
