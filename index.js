2a0a322ba866ca93ca454a3b173987c95c9749e9
TheMattyDGWU <themattydgwu@gmail.com>
2020-10-14 16:59:16 -0400

    deleted test run and updated commit badge

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function generateReadMe(answers) {
 [![GitHub issues](https://img.shields.io/github/issues/TheyCallMeMattyD/readme-generator?style=for-the-badge)](https://github.com/TheyCallMeMattyD/readme-generator/issues) &nbsp;
 [![GitHub stars](https://img.shields.io/github/stars/TheyCallMeMattyD/readme-generator?style=for-the-badge)](https://github.com/TheyCallMeMattyD/readme-generator/stargazers) &nbsp;
 ![GitHub last commit](https://img.shields.io/github/last-commit/theycallmemattyd/readme-generator?style=for-the-badge) &nbsp;
-![GitHub commit activity](https://img.shields.io/github/commit-activity/m/TheyCallMeMattyD/readme-generator?style=for-the-badge)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/TheyCallMeMattyD/readme-generator?style=for-the-badge)  
 
 # ${answers.Title}
 ## Description
@@ -62,21 +62,21 @@ function generateReadMe(answers) {
   
 ## Table of Contents
 >1. [Title](#Title)
-2. [Description](#Description)
-3. [Installation](#Installation)
-4. [Usage](#Usage)
-5. [License](#License)
-6. [Contributing](#Contributing)
-7. [Tests](#Tests)
-8. [Questions?](#Questions?)
+>2. [Description](#Description)
+>3. [Installation](#Installation)
+>4. [Usage](#Usage)
+>5. [License](#License)
+>6. [Contributing](#Contributing)
+>7. [Tests](#Tests)
+>8. [Questions?](#Questions?)
   
 ## Installation
 >1. Be sure you've installed npm (node project manager) and node.js on your system.  
-2. On your terminal command line, enter 'npm i inquirer'. This will ensure the correct node_module is installed. 
-3. To run the generator, enter 'node index.js' ... then just start answering the questions!   
+>2. On your terminal command line, enter 'npm i inquirer'. This will ensure the correct node_module is installed. 
+>3. To run the generator, enter 'node index.js' ... then just start answering the questions!   
   
 ## Usage
->Simply answer each question as they appear and press 'Enter' to proceed to the next question.
+>Simply answer each question as they appear and press 'Enter' to proceed to the next question.  
 ![Usage Screenshot](img/generator1.png)
   
 ## License [![License: ${answers.License}](https://img.shields.io/badge/License-${answers.License}-blue.svg)](https://opensource.org/licenses/${answers.License})
