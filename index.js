77ac62c065b3e0d74679a6a05b3fd30e15a42cae
TheMattyDGWU <themattydgwu@gmail.com>
2020-10-14 18:24:52 -0400

    contributions spacing

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ function generateReadMe(answers) {
   
 ## Contributing
 >Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.  
-
 >Please make sure to update tests as appropriate.
 
 ## Tests
